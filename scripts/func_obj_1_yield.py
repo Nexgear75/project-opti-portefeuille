1d3ccced9adbe877d92b1881c18432aa8f58c87f
Paul-Aimé <paulaime.robert@lilo.org>
2025-12-11 13:34:23 +0100

    fix: function content (f1,f2)

--- scripts/func_obj_1_yield.py
+++ scripts/func_obj_1_yield.py
@@ -4,13 +4,17 @@
     Fougeroux Alex & Robert Paul-Aime
 
 """
+import numpy as np
 
-def get_yield():
+def get_yield(weights, mu):
     """
         process the yield
+        fuction 1 minimize de negative yield (f1(w) = -w^T * mu)
 
         input:
-
+            (list[float]): weights
+            (numpy: array[float]): mu
         output:
+            (list[float]): f1(w)
     """
-    pass
+    return -np.dot(weights,mu)
--- scripts/func_obj_2_risk.py
+++ scripts/func_obj_2_risk.py
@@ -4,13 +4,18 @@
     Fougeroux Alex & Robert Paul-Aime
 
 """
+import numpy as np
 
-def get_risk():
+def get_risk(weights,sigma):
     """
         process risk
+        function 2 minimize the risk (f2(w) = w^T * sigma * w) 
 
         input:
+            (list[float]): weights
+            (numpy: array[array[float]]): sigma
 
         output:
+            (list[float]): f2(w)
     """
-    pass
+    return np.dot(weights.T,np.dot(sigma,weights))
